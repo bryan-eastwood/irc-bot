21210e5826b8787a4eaf1f31e62a91c3e745cb91
Bryan <bryane@protonmail.ch>
2015-06-07 18:15:53 +0000

    Added about.

---
+++ plugins/about.lisp
@@ -0,0 +1,7 @@
+(defcommand ":about" (msg)
+  (declare (ignore msg))
+  (msg "I am a small irc bot written in Common Lisp."))
+
+(defcommand ":source" (msg)
+  (declare (ignore msg))
+  (msg "https://github.com/bryan-eastwood/irc-bot/tree/master/plugins"))
